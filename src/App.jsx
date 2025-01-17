aa0c2c8af952ef11d186c5027f326bd0bb1a5393
Марко <mgusanica@gmail.com>
2025-01-17 20:41:14 +0200

    віаі

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Books } from "components/Books"
-import { Card } from "components/Card"
+import { Greeting } from "components/Greeting"
+import { PaintingList } from "components/PaintingList"
+import data from '../src/data.json'
+import { Message } from "components/Message"
 
 export const App = ()=>{
     return <>
-    <Card/>
-    <Books/>
+    <Message message = 'Це виконане завдання' />
+    {/* <PaintingList data = {data} />
+    <Greeting name = 'Marko'/> */}
     </>
 }
--- src/components/PaintingList.jsx
+++ src/components/PaintingList.jsx
@@ -1,7 +1,6 @@
-import data from '../data.json'
-export const Card = ()=>{
+export const PaintingList = (props)=>{
     return <ul>
-      {data.map((paintCard) => {
+      {props.data.map((paintCard) => {
         return <li key={paintCard.id}>
     <img src={paintCard.url} alt="..." width="480"/>
     <h2 style={{color: 'tomato'}}>{paintCard.title }</h2>
@@ -9,7 +8,7 @@ export const Card = ()=>{
         Автор: <a href={paintCard.author.url}>{ paintCard.author.tag}</a>
     </p>
     <p>Ціна:{paintCard.price} грн</p>
-    <p>Наявність:{! paintCard.quantity < 5 ?'Товар закінчується' : paintCard.quantity}  </p>
+    <p>Наявність:{paintCard.quantity < 5 ?'Товар закінчується' : paintCard.quantity}  </p>
     <button type="button">Додати в кошик</button>
     </li>
     })}
